741b46c66c5750d30e02e212f2a1a05df3926514
Reddy <akiti.bala.murali.krishna.reddy@hp.com>
2021-10-02 16:17:23 +0530

    user authentication done

--- src/api/user.js
+++ src/api/user.js
@@ -5,8 +5,10 @@ const userRoute = express.Router()
 userRoute.route('/')
     .get( async (req,res) => {
         try{
-            const users = await User.find()
-            console.log(`here ${users}`);
+            const { userId } = req.body
+            const user = await User.findOne( { id:userId})
+            console.log(`authenticated ${user}`);
+            res.send(`Welcome ${user.username}`).status(200)
         }catch(err){
             console.log(err)
         }
